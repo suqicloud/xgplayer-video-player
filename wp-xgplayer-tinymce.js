cda087a4cd5f75256cdca330f5829bf96aea13a9
Xsc <59879111@qq.com>
2025-01-07 19:22:30 +0800

    Add files via upload

--- wp-xgplayer-tinymce.js
+++ wp-xgplayer-tinymce.js
@@ -16,8 +16,19 @@
                             minHeight: 100
                         },
                         {
+                            type: 'textbox',
+                            name: 'xgposter_url',
+                            label: '封面图地址 (可选)',
+                            value: '', // 初始值为空
+                            minWidth: 300
+                        },
+                        {
                             type: 'label',
                             text: '请在上面输入视频的完整链接，多个视频就每行一个。'
+                        },
+                        {
+                            type: 'label',
+                            text: '如果需要封面图，填写对应的图片地址，留空就不显示封面。'
                         }
                     ],
                     onsubmit: function(e) {
@@ -25,7 +36,19 @@
                             return url.trim();
                         }).join(',');
 
-                        editor.insertContent('[xgplayer_video url="' + videoUrls + '"]');
+                        // 获取封面图地址，如果填写了，则添加poster参数
+                        var posterUrl = e.data.xgposter_url.trim();
+                        var shortcode = '[xgplayer_video url="' + videoUrls + '"';
+
+                        // 如果封面图地址不为空，添加poster参数
+                        if (posterUrl) {
+                            shortcode += ' poster="' + posterUrl + '"';
+                        }
+
+                        shortcode += ']';
+
+                        // 插入内容
+                        editor.insertContent(shortcode);
                     }
                 });
             }
